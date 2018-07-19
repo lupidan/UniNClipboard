3f79b170e14b3951010cf2708572fddae4905383
Daniel Lupiañez Casares <lupidan@gmail.com>
2018-07-19 20:32:36 +0300

    Moves and adds some test scripts

---
+++ UniNClipboard/Assets/Scripts/TestScript.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Globalization;
+using UniN.Clipboard;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			Clipboard.SharedClipboard.Text = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+		}
+
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			Debug.Log("This is on the clipboard" + Clipboard.SharedClipboard.Text);
+		}
+	}
+}
