110a8e7c9911f218f9340d633464549758288f40
Daniel Lupiañez Casares <lupidan@gmail.com>
2018-07-19 20:20:03 +0300

    Implements a shared clipboard

---
+++ UniNClipboard/Assets/UniNClipboard/SharedClipboard.cs
@@ -0,0 +1,36 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+#endif
+
+namespace UniN.Clipboard
+{
+    public class Clipboard
+    {
+        private static IClipboard _shared;
+        public static IClipboard SharedClipboard
+        {
+            get
+            {
+                if (_shared == null)
+                {
+                    #if UNITY_EDITOR
+                    var gameObject = new GameObject
+                    {
+                        name = "UniNClipboard"
+                    };
+
+                    Object.DontDestroyOnLoad(gameObject);
+                    _shared = gameObject.AddComponent<EditorClipboard>();
+                    #elif UNITY_IOS
+                    _shared = new IOSClipboard();
+                    #elif UNITY_ANDROID
+                    _shared = new AndroidClipboard("UniNClipboard");
+                    #endif
+                }
+
+                return _shared;
+            }
+
+        }
+    }
+}
