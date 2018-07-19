72d8aa654667cd4e020ebd406cbfb62e83df89e3
Daniel Lupiañez Casares <lupidan@gmail.com>
2018-07-19 20:56:57 +0300

    Implements a TestMenu

---
+++ UniNClipboard/Assets/Scripts/TestMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestMenu : MonoBehaviour
+{
+	[SerializeField] private Text _contentLabel;
+
+	public void OnCopyButtonPressed()
+	{
+		Clipboard.SharedClipboard.Text = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+	}
+
+	public void OnDisplayContentPressed()
+	{
+		this._contentLabel.text = Clipboard.SharedClipboard.Text;
+	}
+}
