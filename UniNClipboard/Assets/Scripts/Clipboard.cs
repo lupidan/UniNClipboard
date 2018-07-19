0ebe6be3101469e969d3080a458ee88dccbe33b8
Daniel Lupiañez Casares <lupidan@gmail.com>
2018-07-19 20:44:55 +0300

    Simplify test scripts

--- UniNClipboard/Assets/Scripts/Clipboard.cs
+++ UniNClipboard/Assets/Scripts/Clipboard.cs
@@ -2,35 +2,34 @@
 using UnityEngine;
 #endif
 
-namespace UniN.Clipboard
+using UniN.UniNClipboard;
+
+public class Clipboard
 {
-    public class Clipboard
+    private static IClipboard _shared;
+    public static IClipboard SharedClipboard
     {
-        private static IClipboard _shared;
-        public static IClipboard SharedClipboard
+        get
         {
-            get
+            if (_shared == null)
             {
-                if (_shared == null)
+                #if UNITY_EDITOR
+                var gameObject = new GameObject
                 {
-                    #if UNITY_EDITOR
-                    var gameObject = new GameObject
-                    {
-                        name = "UniNClipboard"
-                    };
+                    name = "UniNClipboard"
+                };
 
-                    Object.DontDestroyOnLoad(gameObject);
-                    _shared = gameObject.AddComponent<EditorClipboard>();
-                    #elif UNITY_IOS
-                    _shared = new IOSClipboard();
-                    #elif UNITY_ANDROID
-                    _shared = new AndroidClipboard("UniNClipboard");
-                    #endif
-                }
-
-                return _shared;
+                Object.DontDestroyOnLoad(gameObject);
+                _shared = gameObject.AddComponent<EditorClipboard>();
+                #elif UNITY_IOS
+                _shared = new IOSClipboard();
+                #elif UNITY_ANDROID
+                _shared = new AndroidClipboard("UniNClipboard");
+                #endif
             }
 
+            return _shared;
         }
+
     }
 }
--- UniNClipboard/Assets/Scripts/TestScript.cs
+++ UniNClipboard/Assets/Scripts/TestScript.cs
@@ -1,7 +1,6 @@
 ﻿
 using System;
 using System.Globalization;
-using UniN.Clipboard;
 using UnityEngine;
 
 public class TestScript : MonoBehaviour
