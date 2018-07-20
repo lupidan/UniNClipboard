1d039ea80bf1ff9b6c4dc8adf8ea1b29481adb16
Daniel Lupiañez Casares <lupidan@gmail.com>
2018-07-21 00:25:38 +0300

    Potential OSX implementation

---
+++ UniNClipboard/Assets/UniN/UniNClipboard/OSX/OSXClipboard.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace UniN.UniNClipboard
+{
+	public class OSXClipboard : IClipboard
+	{
+		public bool ClipboardAvailable
+		{
+			get { return true; }
+		}
+
+		public string Text
+		{
+			get { return OSXUniNClipboardGetText(); }
+			set { OSXUniNClipboardSetText(value); }
+		}
+
+		[DllImport("UniNClipboard")]
+		private static extern string OSXUniNClipboardGetText();
+
+		[DllImport("UniNClipboard")]
+		private static extern void OSXUniNClipboardSetText(string text);
+	}
+}
+
