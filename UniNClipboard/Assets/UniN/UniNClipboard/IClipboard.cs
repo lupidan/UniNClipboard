10b4610db58daf86cc82eda70835e74f81dcaaa9
Daniel Lupiañez Casares <lupidan@gmail.com>
2018-08-04 19:36:28 +0300

    Adds event to interface

--- UniNClipboard/Assets/UniN/UniNClipboard/IClipboard.cs
+++ UniNClipboard/Assets/UniN/UniNClipboard/IClipboard.cs
@@ -1,9 +1,12 @@
-﻿
+﻿using System;
+
 namespace UniN.UniNClipboard
 {
 	public interface IClipboard
 	{
 		bool ClipboardAvailable { get; }
 		string Text { get; set; }
+
+		event Action OnClipboardChanged;
 	}
 }
