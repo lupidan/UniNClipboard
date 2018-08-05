d67c556e0439204b9f81cd7f1ba29288aadea1a0
Daniel Lupiañez Casares <lupidan@gmail.com>
2018-08-05 12:02:54 +0300

    Adds test code for listeners

--- UniNClipboard/Assets/Scripts/TestMenu.cs
+++ UniNClipboard/Assets/Scripts/TestMenu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +7,9 @@ using UnityEngine.UI;
 public class TestMenu : MonoBehaviour
 {
 	[SerializeField] private Text _contentLabel;
+
+	private int listenerId = 1;
+	private readonly List<Action> registeredListeners = new List<Action>();
 
 	public void OnCopyButtonPressed()
 	{
@@ -15,5 +19,29 @@ public class TestMenu : MonoBehaviour
 	public void OnDisplayContentPressed()
 	{
 		this._contentLabel.text = Clipboard.SharedClipboard.Text;
+	}
+
+	public void OnAddListenerButtonPressed()
+	{
+		var assignedListenerId = this.listenerId;
+		this.listenerId += 1;
+
+		Action listenerToAdd = () =>
+		{
+			var localListenerId = assignedListenerId;
+			Debug.Log(string.Format("[{0}]Clipboard changed: {1}", localListenerId, Clipboard.SharedClipboard.Text));
+		};
+		Clipboard.SharedClipboard.OnClipboardChanged += listenerToAdd;
+		this.registeredListeners.Add(listenerToAdd);
+	}
+
+	public void OnRemoveListenerButtonPressed()
+	{
+		if (this.registeredListeners.Count == 0)
+			return;
+
+		var listenerToRemove = this.registeredListeners[this.registeredListeners.Count - 1];
+		Clipboard.SharedClipboard.OnClipboardChanged -= listenerToRemove;
+		this.registeredListeners.Remove(listenerToRemove);
 	}
 }
