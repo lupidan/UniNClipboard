2eb2681c3f0aa8667df84a52584a3636527c9e62
Daniel Lupiañez Casares <lupidan@gmail.com>
2018-08-06 23:45:15 +0300

    Adds basic unitypackage generation

---
+++ UniNClipboard/Assets/UniN/UniNClipboard/EditorTools/UnityPackageMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace UniN.UniNClipboard
+{
+	public class UnityPackageMenu : MonoBehaviour
+	{
+		[MenuItem("UniN/UniNClipboard/Generate unitypackage")]
+		private static void NewMenuOption()
+		{
+			AssetDatabase.ExportPackage(
+				"Assets/UniN/UniNClipboard",
+				"../UniNClipboard.unitypackage",
+				ExportPackageOptions.Recurse);
+		}
+	}
+}
